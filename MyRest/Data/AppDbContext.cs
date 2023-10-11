a2b77ebb7a6d2bde57016b6ba857d5c5622a21ed
abdumutal <abdumutalmahamatqulov6@gmail.com>
2023-10-11 18:12:11 +0500

    Add project files.

---
+++ MyRest/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyRest.Entities;
+
+namespace MyRest.Data;
+
+public class AppDbContext:DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options):base(options) { }
+    public DbSet<Users> Users { get; set; }
+    public DbSet<Course> courses { get; set; }
+}
